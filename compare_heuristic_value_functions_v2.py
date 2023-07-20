126f034ff3bfdeec2151b1e6d9a85b474d89ba86
is541 <is541@cam.ac.uk>
2023-07-20 20:09:02 +0100

    Modified new script for comparison

--- compare_heuristic_value_functions_v2.py
+++ compare_heuristic_value_functions_v2.py
@@ -14,6 +14,10 @@ import json
 import sys
 from typing import Any
 from tqdm import tqdm
+import os
+import torch
+import deepchem as dc
+
 
 from syntheseus.search.chem import Molecule
 from syntheseus.search.node_evaluation.common import ConstantNodeEvaluator
@@ -30,7 +34,14 @@ from compare_heuristic_value_functions_v1 import (
     FiniteMolIsPurchasableCost,
 )
 from faster_retro_star import ReduceValueFunctionCallsRetroStar
-from neighbour_value_functions import DistanceToCost
+from value_functions import initialize_value_functions, ConstantMolEvaluator
+from embedding_model import (
+    FingerprintModel,
+    GNNModel,
+    load_embedding_model_from_pickle,
+    load_embedding_model_from_checkpoint,
+)
+
 
 
 def get_parser():
@@ -48,7 +59,7 @@ def get_parser():
     parser.add_argument(
         "--limit_rxn_model_calls",
         type=int,
-        default=25,
+        default=5000,
         help="Allowed number of calls to reaction model.",
     )
     parser.add_argument(
@@ -60,7 +71,8 @@ def get_parser():
     parser.add_argument(
         "--and_node_cost_function",
         type=str,
-        default="constant-1",
+        # default="constant-1",
+        required=True,
         help="Which cost function to use for AND nodes.",
     )
     return parser
@@ -157,9 +169,25 @@ if __name__ == "__main__":
         filemode="w",
     )
     logging.getLogger().info(args)
+    
+    # Create output folder
+    if args.input_file=="Guacamol_100_hardest_to_solve":
+        eventid = f'202306-3017-5132-d4cd54bf-e5a4-44c5-82af-c629a3692d87_HARDEST_v2_cost_{args.and_node_cost_function}'
+    if args.input_file=="Guacamol_100_hard_to_solve":
+        eventid = f"202307-0320-4900-d4c27728-a5aa-4177-8681-268a17c3d208_HARD_v2_cost_{args.and_node_cost_function}"
+    elif args.input_file=="Guacamol_100_mid_hard_to_solve":
+        eventid = f'202307-0620-3725-cc7b1f07-14cd-47e8-9d40-f5b2f358fa28_MID_HARD_v2_cost_{args.and_node_cost_function}'
+    elif args.input_file=="Guacamol_100_mid_to_solve":
+        eventid = f'MID_v2_cost_{args.and_node_cost_function}'
+    elif args.input_file=="Guacamol_100_mid_easy_to_solve":
+        eventid = f'MID_EASY_v2_cost_{args.and_node_cost_function}'
+    output_folder = f"CompareTanimotoLearnt/{eventid}"
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     # Load all SMILES to test
-    with open(args.smiles_file, "r") as f:
+    with open(f"Data/{args.input_file}.txt", "r") as f:
         test_smiles = [line.strip() for line in f.readlines()]
 
     # Make reaction model, inventory, value functions
@@ -176,37 +204,100 @@ if __name__ == "__main__":
         raise NotImplementedError(args.and_node_cost_function)
 
     # Create all the value functions to test
-    value_fns = [  # baseline: 0 value function
-        ("constant-0", ConstantNodeEvaluator(0.0)),
+
+    
+    value_fns_names = [
+        'constant-0',
+        'Tanimoto-distance',
+        # 'Tanimoto-distance-TIMES0',
+        # 'Tanimoto-distance-TIMES001',
+        # 'Tanimoto-distance-TIMES01',
+        # 'Tanimoto-distance-TIMES03',
+        # 'Tanimoto-distance-TIMES10',
+        # 'Tanimoto-distance-TIMES100',
+        # 'Tanimoto-distance-TIMES1000',
+        # 'Tanimoto-distance-EXP',
+        # 'Tanimoto-distance-SQRT',
+        # 'Tanimoto-distance-NUM_NEIGHBORS_TO_1',
+        # "Embedding-from-fingerprints-TIMES01",
+        # "Embedding-from-fingerprints-TIMES03",
+        # "Embedding-from-fingerprints",
+        # "Embedding-from-fingerprints-TIMES10",
+        # "Embedding-from-fingerprints-TIMES100",
+        # "Embedding-from-fingerprints-TIMES1000",
+        # "Embedding-from-fingerprints-TIMES10000",
+        # "Embedding-from-gnn-TIMES01",
+        # "Embedding-from-gnn-TIMES03",
+        # "Embedding-from-gnn",
+        # "Embedding-from-gnn-TIMES10",
+        # "Embedding-from-gnn-TIMES100",
+        # "Embedding-from-gnn-TIMES1000",
+        # "Embedding-from-gnn-TIMES10000",
     ]
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Nearest neighbour cost heuristics
-    for num_nearest_neighbours in [1]:
-        for scale in [1.0]:
-            # Tanimoto distance cost heuristic
-            value_fns.append(
-                (
-                    f"Tanimoto-top{num_nearest_neighbours}NN-linear-{scale}",
-                    ScaledTanimotoNNAvgCostEstimator(
-                        scale=scale,
-                        inventory=inventory,
-                        distance_to_cost=DistanceToCost.NOTHING,
-                        num_nearest_neighbours=num_nearest_neighbours,
-                        nearest_neighbour_cache_size=100_000,
-                    ),
-                )
-            )
+    # Fingerprint model
 
-    # SAscore cost heuristic (different scale)
-    for scale in [1.0]:
-        value_fns.append(
-            (
-                f"SAscore-linear-{scale}",
-                ScaledSAScoreCostFunction(
-                    scale=scale,
-                ),
-            )
-        )
+    model_fnps, config_fnps = load_embedding_model_from_checkpoint(
+        experiment_name=args.fnp_embedding_model_to_use,
+        device=device,
+        checkpoint_name="epoch_11_checkpoint",
+    )
+
+    # Graph model
+    model_gnn, config_gnn = load_embedding_model_from_checkpoint(
+        experiment_name=args.gnn_embedding_model_to_use,
+        device=device,
+        checkpoint_name="epoch_76_checkpoint",
+    )
+
+    distance_type_fnps = "cosine"
+    distance_type_gnn = "cosine"
+    featurizer_gnn = dc.feat.MolGraphConvFeaturizer()
+
+    value_fns = initialize_value_functions(
+        value_fns_names=value_fns_names,
+        inventory=inventory,
+        model_fnps=model_fnps,
+        distance_type_fnps=distance_type_fnps,
+        model_gnn=model_gnn,
+        distance_type_gnn=distance_type_gnn,
+        featurizer_gnn=featurizer_gnn,
+        device=device,
+    )
+
+    # value_fns = [  # baseline: 0 value function
+    #     ("constant-0", ConstantNodeEvaluator(0.0)),
+    # ]
+    # # Nearest neighbour cost heuristics
+    # for num_nearest_neighbours in [1]:
+    #     for scale in [1.0]:
+    #         # Tanimoto distance cost heuristic
+    #         value_fns.append(
+    #             (
+    #                 f"Tanimoto-top{num_nearest_neighbours}NN-linear-{scale}",
+    #                 ScaledTanimotoNNAvgCostEstimator(
+    #                     scale=scale,
+    #                     inventory=inventory,
+    #                     distance_to_cost=DistanceToCost.NOTHING,
+    #                     num_nearest_neighbours=num_nearest_neighbours,
+    #                     nearest_neighbour_cache_size=100_000,
+    #                 ),
+    #             )
+    #         )
+    
+
+    # # SAscore cost heuristic (different scale)
+    # for scale in [1.0]:
+    #     value_fns.append(
+    #         (
+    #             f"SAscore-linear-{scale}",
+    #             ScaledSAScoreCostFunction(
+    #                 scale=scale,
+    #             ),
+    #         )
+    #     )
 
     # Run each value function
     overall_results = dict(
@@ -223,5 +314,5 @@ if __name__ == "__main__":
     )
 
     # Save results
-    with open(args.output_json, "w") as f:
+    with open(f"{output_folder}/{args.output_json}", "w") as f:
         json.dump(overall_results, f, indent=2)
